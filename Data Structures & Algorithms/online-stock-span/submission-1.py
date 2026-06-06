a40f6a6df164b6b114246820560c25eb4964b772
José Enríquez <142114522+joseenriquez1404@users.noreply.github.com>
2026-06-06 16:20:34 -0600

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class StockSpanner:
+
+    def __init__(self):
+        self.prices = deque()
+
+    def next(self, price: int) -> int:
+        self.prices.append(price)
+        span = 0
+        for i in range(len(self.prices) - 1, -1, -1):
+            if self.prices[i] <= price:
+                span += 1
+            else:
+                break
+        return span
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+
+"""
+Span -> Numero maximo de dias consecutivos 
+desde ese dia para atras donde el precio de la accion era menor o igual
+"""
