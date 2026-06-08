0381a37bfa847df626f73b82eea1b1d994a6fb3f
José Enríquez <142114522+joseenriquez1404@users.noreply.github.com>
2026-06-08 12:39:57 -0600

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        ans = float('-inf')
+
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid <= x:
+                ans = max(ans, mid)
+                l += 1
+            elif mid * mid > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return ans
